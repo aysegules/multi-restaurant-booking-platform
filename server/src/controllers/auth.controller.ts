0edfd083c62c96776b77fa5728cb2a3e989cc598
aysegul <ayseguleskk@gmail.com>
2026-09-15 22:26:32 +0300

    implement user registration and login functionality

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -1,9 +1,27 @@
 import type { Request, Response } from "express";
 import { asyncHandler } from "../utils/asyncHandler";
+import * as authService from "../services/auth.service";
 
-const register = asyncHandler(async (req: Request, res: Response) => {});
+const register = asyncHandler(async (req: Request, res: Response) => {
+  const { name, email, password, phone } = req.body;
 
-const login = asyncHandler(async (req: Request, res: Response) => {});
+  const user = await authService.register({ name, email, password, phone });
 
+  res.status(201).json({
+    status: "success",
+    data: user,
+  });
+});
+
+const login = asyncHandler(async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+
+  const { user, token } = await authService.login({ email, password });
+
+  res.status(200).json({
+    status: "success",
+    data: { token, user },
+  });
+});
 
 export { register, login };
